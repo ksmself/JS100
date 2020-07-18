8b9d53ab47d71a29ce7f5d0235d174f23ef0db44
ksmself <53217988+ksmself@users.noreply.github.com>
2020-07-19 02:10:16 +0900

    Solved 1-11.

---
+++ 1-11.js
@@ -0,0 +1,14 @@
+/*
+1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. for를 사용해야 합니다.
+
+let s = 0;
+
+//pass
+
+console.log(s);
+*/
+
+//My Answer was same with real answer.  
+for(let i = 1; i < 101; i++){
+  s += i; 
+}
