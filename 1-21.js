055b34bfbd1b9b5a378a1512129c05161a539cc0
ksmself <53217988+ksmself@users.noreply.github.com>
2020-07-20 19:26:57 +0900

    Solved 1-21.

---
+++ 1-21.js
@@ -0,0 +1,13 @@
+/*
+다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+1)  var x = {1, 2, 3, 5, 6, 7};
+2)  var x = {};
+3)  var x = new Set('javascript');
+4)  var x = new Set(range(5));
+5)  var x = new Set();
+*/
+
+//My Answer : 5번 
+
+//Answer: 3번과 5번, 3번의 경우 x가 'javascript'를 가진 채로 생성된다. 
