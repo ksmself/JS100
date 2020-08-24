abbc47a73b94fb9d51e25850e650c9f629c8845c
ksmself <53217988+ksmself@users.noreply.github.com>
2020-08-24 23:02:21 +0900

    Solved 2-21.

---
+++ 2-21.js
@@ -0,0 +1,81 @@
+/*
+깊이 우선 탐색이란 목표한 노드를 찾기 위해 가장 우선순위가 높은 노드의 자식으로 깊이 들어갔다가 목표 노드가 존재하지 않으면 처음 방문한 노드와 연결된 다른 노드부터 
+그 자식 노드로 파고드는 검색 방법을 말합니다. 다음과 같이 리스트 형태로 노드들의 연결 관계가 주어진다고 할 때 깊이 우선 탐색으로 이 노드들을 탐색했을 때의 순서를 공백으로 구분하여 출력하세요.
+
+데이터
+graph = {'E': ['D', 'A'],
+         'F': ['D'],
+         'A': ['E', 'C', 'B'],
+         'B': ['A'],
+         'C': ['A'],
+         'D': ['E','F']}
+
+출력
+E D F A C B
+*/ 
+
+//My Answer
+function dfs(){
+  const graph = {
+    'E': ['D', 'A'],
+    'F': ['D'],
+    'A': ['E', 'C', 'B'],
+    'B': ['A'],
+    'C': ['A'],
+    'D': ['E','F']
+  }; 
+  let res = Object.keys(graph)[0]; 
+  let curKey = Object.keys(graph)[0];
+  let curVal; 
+  let returnKey; 
+  while(res.length !== 6){
+    curVal = graph[curKey];
+    for(let i = 0; i < curVal.length; i++){
+      if(!res.includes(curVal[i]) && i !== curVal.length - 1){
+        returnKey = curKey; 
+      }
+      if(!res.includes(curVal[i])){
+        res += curVal[i]; 
+        curKey = curVal[i]; 
+        break; 
+      }
+      if(i == curVal.length - 1){
+        curKey = returnKey;
+      } 
+    } 
+  }
+  return res.split("").join(" ");
+}
+
+console.log(dfs());
+
+//Answer
+const graph = {
+  'A': ['E', 'C', 'B'],
+  'B': ['A'],
+  'C': ['A'],
+  'D': ['E','F'],
+  'E': ['D', 'A'],
+  'F': ['D'],
+};
+
+
+function dfs(graph, start){
+  let visited = [];
+  let stack = [start];
+
+  while (stack.length !== 0){
+    let n = stack.pop();
+    if (!visited.includes(n)){
+      visited.push(n);
+      let sub = graph[n].filter(x => !visited.includes(x));
+      for(let i of sub) {
+        stack.push(i);
+      }
+    }
+  }
+  return visited;
+}
+
+
+console.log(dfs(graph, 'E'));
